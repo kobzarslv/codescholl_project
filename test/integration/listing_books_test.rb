b8d2e216e4becef3f28be88e4f65f689861075f9
@kobzarslv <kobzar.slv@gmail.com>
2017-02-28 23:59:28 +0200

    List books with rating filter. Add Spring to make development faster

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -1,22 +1,29 @@
 require 'test_helper'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
+  
   setup do
   Book.create!(title: 'Pragmatic Programmer', rating: 5)  
   Book.create!(title: "Ender's Game", rating: 4)  
   end
   
 
+
   test 'listing books' do 
     get '/books'
     assert_equal 200, response.status
     assert_equal Mime[:json], response.content_type
+    
     assert_equal Book.count, JSON.parse(response.body).size
   end
-    
-  
-  
+
+  test 'lists top rated books' do
+  get '/books?rating=5'
+  assert_equal 200, response.status
+  assert_equal Mime[:json], response.content_type
   # test "the truth" do
+  assert_equal 1,JSON.parse(response.body).size
+  end  
   #   assert true
-  # end
 end
+
