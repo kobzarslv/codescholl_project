3efbfd4efb51e82b3d759b2ca9cc7036355ab662
@kobzarslv <kobzar.slv@gmail.com>
2017-03-02 19:36:18 +0200

    Extract json helper method

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -14,7 +14,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime[:json], response.content_type
     
-    assert_equal Book.count, JSON.parse(response.body).size
+    assert_equal Book.count, json(response.body).size
   end
 
   test 'lists top rated books' do
@@ -22,7 +22,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
   assert_equal 200, response.status
   assert_equal Mime[:json], response.content_type
   # test "the truth" do
-  assert_equal 1,JSON.parse(response.body).size
+  assert_equal 1, json(response.body).size
   end  
   #   assert true
 end
